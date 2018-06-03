8790aff76c7cba5c210b847fb952c3e92ed70d03
Angus <angusclark93@gmail.com>
2018-06-03 19:25:30 +0100

    draw second rectangle - first level

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 (function(){
 
   const canvas = document.querySelector('canvas');
+  const container = document.getElementById('container');
   const ctx = canvas.getContext('2d');
 
   // set canvas size
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+canvas.width = container.clientWidth;
+canvas.height = container.clientHeight;
 
-  // create rectangle
-  ctx.fillRect(50,50,100,100);
+  // create rectangles
+  ctx.fillStyle = 'green';
+  ctx.fillRect(410,250,200,40);
+  ctx.fillRect(850,100,200,40);
+
+//console.dir(container);
 })();
